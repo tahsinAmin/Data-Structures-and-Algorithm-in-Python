38f449cc4e2a38fd4360b7f68042b41cec61c0e2
tahsinAmin <muhammadtahsinamin@gmail.com>
2021-07-27 02:44:49 +0600

    1.2) is_even(k)

--- R1_1_is_multiple.py
+++ R1_1_is_multiple.py
@@ -1,3 +1,8 @@
+# 1.1) Write a short Python function, is multiple(n, m), that takes two integer
+# values and returns True if n is a multiple of m, that is, n = mi for some
+# integer i, and False otherwise.
+
+"""
 def is_multiple(n, m):
     for i in range(n):
         # print("Value of i is %d" %i)
@@ -10,3 +15,18 @@ def is_multiple(n, m):
 n, m = list(map(int, input().split()))
 flag = is_multiple(n, m)
 print(flag)
+"""
+
+# 1.2) Write a short Python function, is even(k), that takes an integer value and
+# returns True if k is even, and False otherwise. <em>However, your function
+# cannot use the multiplication, modulo, or division operators.</em>
+
+"""
+def is_even(n):
+    return True if (-1)**n == 1 else False
+
+n = int(input())
+print(is_even(n))
+"""
+
+# https://github.com/RichardAfolabi/Data-Structures-Algorithms-in-Python/blob/master/chp_one_python_primer.py
