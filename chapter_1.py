bbf834f39aa19f3a6d7fe55bfea7fc6b88c185a3
tahsinAmin <muhammadtahsinamin@gmail.com>
2021-08-16 19:45:33 +0600

    C-1.21
    list.reverse()

--- chapter_1.py
+++ chapter_1.py
@@ -238,15 +238,20 @@ if __name__ == '__main__':
 		print('shuffled:',a)
 """
 
+# C-1.21 Write a Python program that repeatedly reads lines from standard input
+# until an EOFError is raised, and then outputs those lines in reverse order
+# (a user can indicate end of input by typing ctrl-D)
 """
 a=[]
 while True:
 	try:
-		n = int(input())
+		n = input()
 		a.append(n)
 	except(EOFError):
-		print("hello")
-		
+		break
+	
+a.reverse()
+[print(i) for i in a]
 """
 
 
