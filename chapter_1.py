0664bec5aea7eb3e642841708b5f9891e526b2cf
tahsinAmin <muhammadtahsinamin@gmail.com>
2021-08-23 18:03:33 +0600

    c-1.28
    call method([1,2,7]) => def method(l): => for i in v: do this...

--- chapter_1.py
+++ chapter_1.py
@@ -316,6 +316,28 @@ if __name__ == '__main__':
 	print('Is {} = {} * {}?'.format(a, b, c), a == b * c)
 """
 
+# C-1.28 The p-norm of a vector v = (v1,v2,...,vn) in n-dimensional space is defined as
+# v = # p # v# p# 1 +v# p# 2 +···+v# p# n .
+# For the special case of p = 2, this results in the traditional Euclidean
+# norm, which represents the length of the vector. For example, the Euclidean norm of a two-dimensional vector with coordinates (4,3) has a
+# Euclidean norm of √
+# 42 +32 = √16+9 = √
+# 25 = 5. Give an implementation of a function named norm such that norm(v, p) returns the p-norm
+# value of v and norm(v) returns the Euclidean norm of v. You may assume
+# that v is a list of numbers.
+
+"""
+def norm(v,p):
+	s = 0
+	for i in v:
+		s+= i**p
+		
+	print(s**(1/p))
+		
+		
+norm([3,4],2)
+"""
+
 # file:///C:/Users/User/Downloads/Documents/Data%20Structures%20and%20Algorithms%20in%20Python%20(%20PDFDrive%20).pdf
 # https://github.com/findmyway/Data-Structures-and-Algorithms-in-Python/tree/master/ch1
 # https://github.com/wdlcameron/Solutions-to-Data-Structures-and-Algorithms-in-Python/blob/master/Chapter%201%20Exercises.ipynb
