78f75d73f5cdef9fd56f661f0e9e60239849683e
tahsinAmin <muhammadtahsinamin@gmail.com>
2021-11-12 14:18:52 +0600

    Binary Search

--- Binary_Search.py
+++ Binary_Search.py
@@ -1,4 +1,22 @@
 def binary_search(data, target, low, high):
+    """Return True if target is found in indecated portion of a Python list.
+
+    The search only considers the protion from data[low] to data[high] inclusive.
+    """
+
+    if low > high:
+        return False  # Interval is empty
+    else:
+        mid = (low+high) // 2
+        if target == data[mid]:
+            # print(mid)
+            return True
+        elif target < data[mid]:
+            # recur on the potion left of the middle
+            return binary_search(data, target, low, mid-1)
+        else:
+            # recur on the portion right of the iddle
+            return binary_search(data, target, mid+1, high)
 
 
 a = [2, 4, 5, 7, 8, 9, 12, 14, 17, 19, 22, 25, 27, 28, 33, 37]
